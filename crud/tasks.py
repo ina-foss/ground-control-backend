9dfa3352a3a42c41bd6429b7f4d06e7e6f5fb942
Pablo <pratouit-prestataire@ina.fr>
2024-04-29 15:01:22 +0200

    fix task get crud

--- crud/tasks.py
+++ crud/tasks.py
@@ -4,4 +4,4 @@ from sqlalchemy.orm import joinedload
 from models.tasks_model import *
 
 def get_task_by_id(db: Session, taskid: int ):
-    return db.query(Task).filter(Task.taskid == taskid).first()
+    return db.query(Task).filter(Task.id == taskid).first()
