df47ba634ef266ef8cc42e6215e227c7e278fa73
belkays <belkays@ina.fr>
2024-05-29 11:58:27 +0200

    variables for db connection

--- src/database.py
+++ src/database.py
@@ -1,8 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = 'postgresql://user:postgres@db:5432/ground_control_db'
+load_dotenv()
+
+PG_SERVER = os.getenv('PG_SERVER')
+PG_DATABASE = os.getenv('PG_DATABASE')
+PG_USERNAME = os.getenv('PG_USERNAME')
+PG_PASSWORD = os.getenv('PG_PASSWORD')
+PG_PORT = os.getenv('PG_PORT')
+
+DATABASE_URL = f'{PG_SERVER}://{PG_USERNAME}:{PG_PASSWORD}@db:{PG_PORT}/{PG_DATABASE}'
 
 engine = create_engine(DATABASE_URL)
 
