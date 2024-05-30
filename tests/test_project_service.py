88b9ee5c83c8bd5bd74275f6e452d98870fb3c09
Pablo <pratouit-prestataire@ina.fr>
2024-05-30 15:22:27 +0200

    delete and get_projects tested

--- tests/test_project_service.py
+++ tests/test_project_service.py
@@ -27,6 +27,33 @@ def db(db_engine):
     transaction.rollback()
     connection.close()
 
+def test_get_projects(db:Session):
+    project_data_1 = {
+        "title": "Test Project 1",
+        "description" : "Test description 2",
+        "created_by" : 1,
+    }
+    project_data_2 = {
+        "title": "Test Project 2 ",
+        "description" : "Test description 2",
+        "created_by" : 2,
+    }
+
+    created_project_1 = create_project_crud(db,ProjectBaseDto(**project_data_1))
+    created_project_2 = create_project_crud(db,ProjectBaseDto(**project_data_2))
+
+    retrieved_projects = get_projects(db)
+
+    assert retrieved_projects is not None
+    assert retrieved_projects[0].id == created_project_1.id
+    assert retrieved_projects[0].description == project_data_1["description"]
+    assert retrieved_projects[0].created_by == project_data_1["created_by"]
+    assert retrieved_projects[0].title == project_data_1["title"]
+    assert retrieved_projects[1].title == project_data_2["title"]
+    assert retrieved_projects[1].description == project_data_2["description"]
+    assert retrieved_projects[1].created_by == project_data_2["created_by"]
+    assert retrieved_projects[1].id == created_project_2.id
+
 def test_get_project_by_id(db:Session):
     project_data = {
         "title": "Test Project",
@@ -79,3 +106,18 @@ def test_update_project_crud(db:Session):
     assert retrieved_updated_project.title == updated_task_data["title"]
     assert retrieved_updated_project.description == updated_task_data["description"]
     assert retrieved_updated_project.created_by == updated_task_data["created_by"]
+
+def test_delete_project_crud(db:Session):
+    project_data = {
+        "title": "Test Project",
+        "description" : "Test description",
+        "created_by" : 1,
+    }
+    created_project = create_project_crud(db,ProjectBaseDto(**project_data))
+
+    delete_project_crud(db,created_project.id)
+
+    retrieved_project = get_project_by_id(db,created_project.id)
+
+    assert created_project is not None
+    assert retrieved_project is None
