2fe3414d2d3f8cb3d60ade0fe4cee58e4082060d
aawayni <aawayni-prestataires@ina.fr>
2024-08-07 22:29:10 +0200

    fix settings_files

--- ina_ground_control/config.py
+++ ina_ground_control/config.py
@@ -6,7 +6,7 @@ from dynaconf import Dynaconf
 
 settings = Dynaconf(
     root_path="ina_ground_control",
-    settings_files=["settings.*.yaml", ".secrets.yaml"],
+    settings_files=["settings*.yaml", ".secrets.yaml"],
     envvar_prefix="INA_GROUND_CONTROL",
     env_switcher="INA_GROUND_CONTROL_ENV",
     environments=True,
