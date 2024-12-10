5566b618e1ffd07d6904302080e0ec3f1bc561ae
mjelassi <mjelassi-prestataire@ina.fr>
2024-12-10 16:11:57 +0100

    fix lint

--- ina_ground_control/routers/plugins.py
+++ ina_ground_control/routers/plugins.py
@@ -1,4 +1,3 @@
-
 from fastapi import status
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
@@ -14,6 +13,20 @@ router = APIRouter(tags=["plugin"])
 #search plugins
 @router.get("/step/{step_id}/{name}-autocomplete/search", response_model=list[PluginCreate])
 def getPlugins(step_id: int,name:str, db: Session = Depends(get_db)):
+    """
+    Retrieve a list of plugins for a specific step and name.
+
+    Args:
+        step_id (int): The ID of the step for which plugins are to be retrieved.
+        name (str): The name used for plugin autocomplete search.
+        db (Session): The database session dependency.
+
+    Returns:
+        list[PluginCreate]: A list of plugins matching the specified criteria.
+
+    Raises:
+        HTTPException: If no plugins are found for the given parameters.
+    """
 
     plugins = get_plugins(db, step_id=step_id,name=name)
     if plugins is None:
