f3906f388fd1c521c0dc81d4fd55bc1b059d836e
mjelassi <mjelassi-prestataire@ina.fr>
2024-12-10 16:25:22 +0100

    fix lint

--- ina_ground_control/routers/plugins.py
+++ ina_ground_control/routers/plugins.py
@@ -1,9 +1,27 @@
-from fastapi import status
+"""
+Module: plugin_router
+
+Description:
+This module defines the routes for managing plugins within the FastAPI application.
+
+Features:
+- Search for plugins associated with a specific step and name.
+- Handle exceptions when no matching plugins are found.
+- Use dependencies to manage database sessions.
+
+Endpoint:
+- GET /step/{step_id}/{name}-autocomplete/search: Enables searching for specific plugins for a given step and name.
+
+Dependencies:
+- FastAPI for routing and dependency injection.
+- SQLAlchemy for database session management.
+- Latios for logging.
+
+"""
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from latios.log import get_logger
 from ina_ground_control.database import get_db
-from ina_ground_control.models.plugin_model import Plugin
 from ina_ground_control.schemas.plugin_schemas import PluginCreate
 from ina_ground_control.services.plugin_service import get_plugins
 
@@ -12,7 +30,7 @@ router = APIRouter(tags=["plugin"])
 
 #search plugins
 @router.get("/step/{step_id}/{name}-autocomplete/search", response_model=list[PluginCreate])
-def getPlugins(step_id: int,name:str, db: Session = Depends(get_db)):
+def get_plugins(step_id: int,name:str, db: Session = Depends(get_db)):
     """
     Retrieve a list of plugins for a specific step and name.
 
