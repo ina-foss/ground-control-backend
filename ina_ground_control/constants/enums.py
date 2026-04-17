6196de8046486ada34045316690594bf11a5bab9
Hounaida Rekik <hrekik-prestataire@ina.fr>
2026-04-17 09:17:23 +0000

    chore: Add new display_zone

--- ina_ground_control/constants/enums.py
+++ ina_ground_control/constants/enums.py
@@ -70,12 +70,14 @@ class DisplayZone(str, Enum):
     Attributes:
         BLOC (str): Represents a standalone block zone.
         SPAN_MODAL_LEFT (str): Represents a modal that spans the left side.
+        SPAN_MODAL_LEFT_SEGMENT (str): Represents a modal that spans the left side.
         SPAN_MODAL_RIGHT (str): Represents a modal that spans the right side.
         GROUP_MODAL (str): Represents a grouped modal zone for multiple plugins.
     """
 
     BLOC = "bloc"
     SPAN_MODAL_LEFT = "span_modal_left"
+    SPAN_MODAL_LEFT_SEGMENT = "span_modal_left_segment"
     SPAN_MODAL_RIGHT = "span_modal_right"
     GROUP_MODAL = "group_modal"
 
