9aeb3da7cfc1a6c362436ad5c1eb0535fd99c2e0
mjelassi <mjelassi-prestataire@ina.fr>
2026-08-17 15:55:33 +0200

    fix database

---
+++ ina_ground_control/alembic/versions/2766d689b3d4_merge_migration_heads.py
@@ -0,0 +1,26 @@
+"""merge migration heads
+
+Revision ID: 2766d689b3d4
+Revises: 5940cdd6d66d, 80beb50daf87
+Create Date: 2026-08-17 14:56:23.810923
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '2766d689b3d4'
+down_revision: Union[str, None] = ('5940cdd6d66d', '80beb50daf87')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
